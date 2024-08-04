4095b713488348c2868090efc94d4401bba7e1b1
Lee Seung Jin <dltmdwls154@gmail.com>
2024-08-04 22:43:13 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlinservertest/KotlinServerTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinservertest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinServerTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinServerTestApplication>(*args)
+}
