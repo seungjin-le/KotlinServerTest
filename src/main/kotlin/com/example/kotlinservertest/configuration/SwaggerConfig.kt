bece30e1f54b31c1b8fad9331fd29512a9595207
Seungjin Lee <dltmdwls154@gmail.com>
2025-05-30 23:42:57 +0900

    Test

--- src/main/kotlin/com/example/kotlinservertest/configuration/SwaggerConfig.kt
+++ src/main/kotlin/com/example/kotlinservertest/configuration/SwaggerConfig.kt
@@ -19,31 +19,29 @@ import springfox.documentation.spring.web.plugins.Docket
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 @SecurityRequirement(name = "bearerAuth")
 class SwaggerConfig {
-    @Bean
-    fun swaggerApi(): Docket = Docket(DocumentationType.OAS_30)
-        .apiInfo(swaggerInfo())
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("com.example.kotlinservertest.controller"))
-        .paths(PathSelectors.any())
-        .build()
-        .useDefaultResponseMessages(false)
+  @Bean
+  fun swaggerApi(): Docket = Docket(DocumentationType.OAS_30)
+    .apiInfo(swaggerInfo())
+    .select()
+    .apis(RequestHandlerSelectors.basePackage("com.example.kotlinservertest.controller"))
+    .paths(PathSelectors.any())
+    .build()
+    .useDefaultResponseMessages(false)
 
-    private fun swaggerInfo() = ApiInfoBuilder()
-        .title("스웨거 테스트")
-        .description("스웨거 API 테스트")
-        .version("1.0.0")
-        .build()
-    @Bean
-    fun webMvcConfigurer(): WebMvcConfigurer {
-        return object : WebMvcConfigurer {
-            override fun addViewControllers(registry: ViewControllerRegistry) {
-                registry.addRedirectViewController("/", "/swagger-ui/index.html")
-            }
-        }
+  private fun swaggerInfo() = ApiInfoBuilder()
+    .title("스웨거 테스트")
+    .description("스웨거 API 테스트")
+    .version("1.0.0")
+    .build()
+
+  @Bean
+  fun webMvcConfigurer(): WebMvcConfigurer {
+    return object : WebMvcConfigurer {
+      override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addRedirectViewController("/", "/swagger-ui/index.html")
+      }
     }
-
-
-
+  }
 }
 
 
