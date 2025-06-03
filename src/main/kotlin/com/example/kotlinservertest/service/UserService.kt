af7a8a3c1b7bc3379bcfcb7a7d4c80944c411b97
Seungjin Lee <dltmdwls154@gmail.com>
2025-06-03 22:11:19 +0900

    Test

--- src/main/kotlin/com/example/kotlinservertest/service/UserService.kt
+++ src/main/kotlin/com/example/kotlinservertest/service/UserService.kt
@@ -8,23 +8,23 @@ import org.springframework.stereotype.Service
 @Service
 class UserService(private val userRepository: UserRepository, private val passwordEncoder: PasswordEncoder) {
 
-    fun getAllUsers(): List<User> = userRepository.findAll()
+  fun getAllUsers(): List<User> = userRepository.findAll()
 
-    fun getUserById(id: String): User? = userRepository.findById(id).orElse(null)
+  fun getUserById(id: String): User? = userRepository.findById(id).orElse(null)
 
-    fun createUser(user: User): User = userRepository.save(user)
+  fun createUser(user: User): User = userRepository.save(user)
 
-    fun updateUser(id: String, user: User): User? {
-        return if (userRepository.existsById(id)) {
-            userRepository.save(user.copy(id = id))
-        } else {
-            null
-        }
+  fun updateUser(id: String, user: User): User? {
+    return if (userRepository.existsById(id)) {
+      userRepository.save(user.copy(id = id))
+    } else {
+      return null
     }
+  }
 
-    fun deleteUser(id: String) {
-        if (userRepository.existsById(id)) {
-            userRepository.deleteById(id)
-        }
+  fun deleteUser(id: String) {
+    if (userRepository.existsById(id)) {
+      userRepository.deleteById(id)
     }
+  }
 }
